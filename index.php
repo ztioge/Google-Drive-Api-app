ce9dbd035f99c50bd1d171c6338805071bf99878
Egoitz <egoitzu@telefonica.net>
2016-11-08 09:00:01 +0000

    Quitando Tokens

--- index.php
+++ index.php
@@ -5,10 +5,14 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 $client = new Soramugi\GoogleDrive\Client;
 
-$client->setClientId('108898640535-lt7750up3kc5079tm505seie2nmdmdqo.apps.googleusercontent.com');
-$client->setClientSecret('nP-1rnofnic7H2LehwqIoZh9');
+//El clientID lo tengo en un archivo en local
+$client->setClientId('');
 
-$token = '{"access_token":"ya29.Ci-JA7xgd2TFITLuUeL-QzTY5C0dphFnKfVThPcggdmyXogNF6fcAFndnojslWf3pA","expires_in":3600,"refresh_token":"1\/w1Bz11LUl-qHaxZgJo_nV7TSIAi1jYT7_2CxXMSUVa4","token_type":"Bearer","created":1477561005}';
+//EL Client secret lo tengo en un arhivo local.
+$client->setClientSecret('');
+
+//El acces Token lo tengo en un arcgivo en local.
+$token = '{"access_token":"","expires_in":3600,"refresh_token":"1\/w1Bz11LUl-qHaxZgJo_nV7TSIAi1jYT7_2CxXMSUVa4","token_type":"Bearer","created":1477561005}';
 $client->setAccessToken($token);
 
 #Objektua sortzen degu
