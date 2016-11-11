db29ed469ac77e69dd207e000e07a4ed90159a31
Egoitz <egoitzu@telefonica.net>
2016-11-11 12:01:58 +0000

    fin

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <!DOCTYPE html>
 <html>
-   <?php
+    
+        <form action="index.php" method="get">
+            <input type="text" name="titulo"/>
+            <input type="submit" value="Submit" name="submit"/>
+        </form>
+<?php
 require_once __DIR__ . '/vendor/autoload.php';
 
 $client = new Soramugi\GoogleDrive\Client;
@@ -11,7 +16,7 @@ $client->setClientId('');
 //EL Client secret lo tengo en un arhivo local.
 $client->setClientSecret('');
 
-//El acces Token lo tengo en un arcgivo en local.
+//El acces Token lo tengo en un archivo en local.
 $token = '{"access_token":"","expires_in":3600,"refresh_token":"1\/w1Bz11LUl-qHaxZgJo_nV7TSIAi1jYT7_2CxXMSUVa4","token_type":"Bearer","created":1477561005}';
 $client->setAccessToken($token);
 
@@ -22,20 +27,25 @@ $file = new Soramugi\GoogleDrive\File($client);
 //Carpeta bakoitzeko barruan dauden artxiboak listatzen ditugu, ta karpetak ere.
 foreach ($files->listFiles()->getItems() as $item) {
     if (!$item->getLabels()->getTrashed()) {
-         echo '<td>' ."file : {$item->getTitle()}\n". '</td>';
+         echo "file : {$item->getTitle()}\n";
          echo "<br>";
     }
 }
 
-echo "EL archivo buscado:";
-echo "<br>";
-foreach($files->searchTitle('Empresa') as $file) {
-    if ($file->isFolder()) {
-        echo $file->getTitle() . "\n";
-        echo "<br>";
-        echo $file->getMimeType() . "\n";
-        echo "<br>";
-        echo $file->getId() . "\n";
+if(isset($_GET["submit"])){
+    echo "<br>";
+    echo "La carpeta buscada:";
+    echo "<br>";
+    foreach($files->searchTitle($_GET["titulo"]) as $file) {
+        if ($file->isFolder()) {
+            echo $file->getTitle();
+            echo "<br>";
+            echo $file->getMimeType();
+            echo "<br>";
+            echo $file->getId();
+        }else{
+            echo "El titulo que buscas no es una carpeta";
+        }
     }
 }
 ?> 
